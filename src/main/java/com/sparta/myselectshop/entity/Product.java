18a1779220b325137062f7ac5f02b55c19fb1702
subinny2 <116608047+subinny2@users.noreply.github.com>
2024-07-30 11:56:13 +0900

    상품 폴더 설계

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
@@ -37,6 +40,9 @@ public class Product extends Timestamped {
     @ManyToOne(fetch = FetchType.LAZY) // 다대일 단방향설정
     @JoinColumn(name = "user_id", nullable = false) // 관계설정
     private User user;
+
+    @OneToMany(mappedBy = "product") // mappedBy 양방향
+    private List<ProductFolder> productFolderList = new ArrayList<>();
 
     public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
